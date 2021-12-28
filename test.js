abce64c80c9188abcce3c0b598c2aea38922f7a7
Sindre Sorhus <sindresorhus@gmail.com>
2021-12-28 03:04:25 +0100

    Fix CI

--- test.js
+++ test.js
@@ -18,11 +18,11 @@ test('-4', async t => {
 	t.true(isIp.v4(stdout));
 });
 
-test('-4 -h -t 1', async t => {
-	await t.throwsAsync(execa('./cli.js', ['-4', '-h', '-t', '1']));
-});
-
 if (!process.env.CI) {
+	test('-4 -h -t 1', async t => {
+		await t.throwsAsync(execa('./cli.js', ['-4', '-h', '-t', '1']));
+	});
+
 	test('-6', async t => {
 		const {stdout} = await execa('./cli.js', ['-6']);
 		t.true(isIp.v6(stdout));
